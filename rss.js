49c8a3dc7dbe6cf89b79610e8b799b16f0293652
simon3000 <simon3000@163.com>
2019-09-15 20:38:26 +0200

    parse rss result

--- rss.js
+++ rss.js
@@ -1,8 +1,11 @@
+const cheerio = require('cheerio')
 const Parser = require('rss-parser')
 const parser = new Parser()
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 const list = ['day_r18', 'day_male_r18', 'week_r18', 'week_r18g']
+
+const knownPictures = new Set()
 
 const fetchAll = apis => apis.map(api => parser.parseURL(`https://rsshub.app/pixiv/ranking/${api}`))
 
@@ -11,11 +14,23 @@ const fetchAll = apis => apis.map(api => parser.parseURL(`https://rsshub.app/pix
 (async () => {
   while (true) {
     const source = await Promise.all(fetchAll(list))
-    source.forEach(({ items, title }) => {
-      console.log(title)
-      console.log(items.length)
-    })
+    const result = source
+      .flatMap(({ items, title: feed }) => items
+        .filter(({ link }) => {
+          if (knownPictures.has(link)) {
+            return false
+          }
+          knownPictures.add(link)
+          setTimeout(() => knownPictures.delete(link), 1000 * 60 * 60 * 24 * 7)
+          return true
+        })
+        .map(({ author, title, link, content }) => ({ author, title, link, content, feed })))
+      .map(({ content, ...others }) => {
+        const $ = cheerio.load(content)
+        const images = $('img').toArray().map(({ attribs: { src } }) => src)
+        return { ...others, images }
+      })
     console.log('done')
-    await wait(3000)
+    await wait(1000 * 60 * 30)
   }
 })()
