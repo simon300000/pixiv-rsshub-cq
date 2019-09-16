8794bb7f3ae66c282a8b949e772200e6bd448cbe
simon3000 <simon3000@163.com>
2019-09-16 19:41:36 +0200

    prpr

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 /* eslint-disable camelcase */
-const { CQWebSocket } = require('cq-websocket')
+const { CQWebSocket, CQImage, CQText } = require('cq-websocket')
+
+const rss = require('./rss')
+const knownPictures = new Set()
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const bot = new CQWebSocket({
-  host: '202.182.101.186',
-  accessToken: 'BY38d7hwd7',
-  port: 9000
+  host: '127.0.0.1',
+  accessToken: 'Prpr',
+  port: 6700
 })
 
-bot.on('message.private', (_, { message, user_id }) => {
-  bot('send_private_msg', { user_id, message })
+bot.once('socket.connect', async () => {
+  while (true) {
+    const items = await rss(knownPictures)
+    await Promise.all(items
+      .map(({ author, title, link, images, feed }) => [new CQText(`${title} by ${author} from ${feed}\n${link}`), ...images.map(url => new CQImage(url))])
+      .map(message => bot('send_group_msg', {
+        group_id: 829736941,
+        message
+      })))
+    console.log('done', items.length)
+    await wait(1000 * 60 * 30)
+  }
 })
+
+const friends = [2241139100, 3151884870, 179528556]
 
 bot.on('request', async ({ user_id, sub_type, flag, request_type, group_id }) => {
-  if (user_id === 2241139100 && request_type === 'group' && sub_type === 'invite') {
+  if (friends.includes(user_id) && request_type === 'group' && sub_type === 'invite') {
     await bot('set_group_add_request', { flag, sub_type })
     console.log('join group', group_id)
+  }
+  if (friends.includes(user_id) && request_type === 'friend') {
+    await bot('set_friend_add_request', { flag })
+    console.log('add friend', user_id)
   }
 })
 
--- rss.js
+++ rss.js
@@ -2,35 +2,27 @@ const cheerio = require('cheerio')
 const Parser = require('rss-parser')
 const parser = new Parser()
 
-const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 const list = ['day_r18', 'day_male_r18', 'week_r18', 'week_r18g']
-
-const knownPictures = new Set()
 
 const fetchAll = apis => apis.map(api => parser.parseURL(`https://rsshub.app/pixiv/ranking/${api}`))
 
-;
-
-(async () => {
-  while (true) {
-    const source = await Promise.all(fetchAll(list))
-    const result = source
-      .flatMap(({ items, title: feed }) => items
-        .filter(({ link }) => {
-          if (knownPictures.has(link)) {
-            return false
-          }
-          knownPictures.add(link)
-          setTimeout(() => knownPictures.delete(link), 1000 * 60 * 60 * 24 * 7)
-          return true
-        })
-        .map(({ author, title, link, content }) => ({ author, title, link, content, feed })))
-      .map(({ content, ...others }) => {
-        const $ = cheerio.load(content)
-        const images = $('img').toArray().map(({ attribs: { src } }) => src)
-        return { ...others, images }
+module.exports = async knownPictures => {
+  const source = await Promise.all(fetchAll(list))
+  const result = source
+    .flatMap(({ items, title: feed }) => items
+      .filter(({ link }) => {
+        if (knownPictures.has(link)) {
+          return false
+        }
+        knownPictures.add(link)
+        setTimeout(() => knownPictures.delete(link), 1000 * 60 * 60 * 24 * 7)
+        return true
       })
-    console.log('done')
-    await wait(1000 * 60 * 30)
-  }
-})()
+      .map(({ author, title, link, content }) => ({ author, title, link, content, feed })))
+    .map(({ content, ...others }) => {
+      const $ = cheerio.load(content)
+      const images = $('img').toArray().map(({ attribs: { src } }) => src)
+      return { ...others, images }
+    })
+  return result
+}
