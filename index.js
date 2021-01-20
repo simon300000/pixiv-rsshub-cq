207d668ea6aef9d51ab2d976ce26012520c588a5
simon3000 <simon30002021@icloud.com>
2021-01-20 14:39:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const bot = new CQWebSocket({
 const group_id = 123
 
 bot.once('socket.connect', async () => {
+  await wait(1000 * 5)
   await bot('send_group_msg', {
     group_id,
     message: [new CQText('Hi, ich bin bot')]
